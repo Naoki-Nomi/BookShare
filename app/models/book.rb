10b201e8103528bcf13a9c8722d6bd02fedf44f4
Naoki-Nomi <n.naoki.116@gmail.com>
2021-03-15 08:36:12 +0000

    [Update]Bookモデルのバリデーションを更新

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,10 @@ class Book < ApplicationRecord
   belongs_to :user
   belongs_to :genre
 
-  validates :author, presence: true
-  validates :book_title, presence: true
+  validates :author, presence: true, length: { maximum: 30 }
+  validates :book_title, presence: true, length: { maximum: 30 }
   validates :read_date, presence: true
+  validates :note, length: { maximum: 1200 }
 
   def self.search(user_id, search, genre_id, from, to)
     book = Book
