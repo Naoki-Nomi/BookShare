340396a0c431f774fbd54f8355c36ae238bb926f
Naoki-Nomi <n.naoki.116@gmail.com>
2021-03-24 05:17:00 +0000

    [Update]記述内容を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.find_or_create_by(id: 1) do |admin|
- admin.email = "sample@sample.com"
- admin.password = "samplesample"
-end
-
 Genre.create!(
  [
   {
